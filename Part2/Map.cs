2f2add4386f664fb39fe2c7050cfbef43df12f59
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-29 20:19:39 +0100

    Add project files.

---
+++ Part2/Map.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    internal class Map
+    {
+        public char[][] CharMap { get; set; }
+
+        public Map(List<string> lines)
+        {
+            CharMap = new char[lines.Count()][];
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                CharMap[i] = lines[i].ToCharArray();
+            }
+        }
+
+        public int GetHorizontalMirror(int targetErrors)
+        {
+            int mirrorLine = 0;
+            for (int row = 1; row < CharMap.Length; row++)
+            {
+                int errors = 0;
+                int top = row - 1;
+                int bottom = row;
+                while (top >= 0 && bottom < CharMap.Length)
+                {
+                    for (int col = 0; col < CharMap[row].Length; col++)
+                    {
+                        if (CharMap[top][col] != CharMap[bottom][col])
+                        {
+                            errors++;
+                        }
+
+                        if (errors > targetErrors)
+                            break;
+                    }
+
+                    if (errors > targetErrors)
+                        break;
+
+                    top -= 1;
+                    bottom += 1;
+                }
+
+                if (errors == targetErrors)
+                {
+                    mirrorLine = row;
+                    break;
+                }
+            }
+
+            return mirrorLine;
+        }
+
+        public int GetVerticalMirror(int targetErrors)
+        {
+            int mirrorLine = 0;
+            for (int col = 1; col < CharMap[0].Length; col++)
+            {
+                int errors = 0;
+                int left = col - 1;
+                int right = col;
+                while (left >= 0 && right < CharMap[0].Length)
+                {
+                    for (int row = 0; row < CharMap.Length; row++)
+                    {
+                        if (CharMap[row][left] != CharMap[row][right])
+                        {
+                            errors++;
+                        }
+
+                        if (errors > targetErrors)
+                            break;
+                    }
+
+                    if (errors > targetErrors)
+                        break;
+
+                    left -= 1;
+                    right += 1;
+                }
+
+                if (errors == targetErrors)
+                {
+                    mirrorLine = col;
+                    break;
+                }
+            }
+
+            return mirrorLine;
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var maps = new List<Map>();
+            var lineGroup = new List<string>();
+            foreach (var line in lines)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    maps.Add(new Map(lineGroup));
+                    lineGroup.Clear();
+                    continue;
+                }
+                lineGroup.Add(line);
+            }
+            maps.Add(new Map(lineGroup));
+
+            var sum1 = maps.Sum(m => m.GetVerticalMirror(0)) + maps.Sum(m => 100 * m.GetHorizontalMirror(0));
+            var sum2 = maps.Sum(m => m.GetVerticalMirror(1)) + maps.Sum(m => 100 * m.GetHorizontalMirror(1));
+
+            Console.WriteLine($"Part 1: {sum1}");
+            Console.WriteLine($"Part 2: {sum2}");
+        }
+    }
+}
